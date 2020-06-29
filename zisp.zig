2b040507fb3493963a6f66630514cbde00451a13
tgt <tgt@specialminds.dk>
2020-06-29 11:01:26 +0200

    Parse to Tokens The parsed objects are now turned into a lexical tree.

--- zisp.zig
+++ zisp.zig
@@ -1,32 +1,36 @@
 const std = @import("std");
 
-pub fn main() !void
+pub fn main() anyerror!void
 {
     const stdOut = std.io.getStdOut().outStream();
 
 //    
 //    try stdOut.print("{}\n", .{ Program.rawCodeString });
 //
-//    try Program.printTo(stdOut);
-//    try stdOut.print("{}\n", .{ Program._arr.len });
+    try Program.printTo(stdOut);
+    try stdOut.print("{}\n", .{ Program.outerTokens.len });
 //
-    try stdOut.print("{}\n", .{ Program.totalCount });
+    try stdOut.print("{}\n", .{ Program.tokenCount });
 //
 //    for (Program._arrStackLens) |len|try stdOut.print("{} ", .{ len });
 
 }
 
-const lisp = @embedFile("lisp.zisp");
-
+const lisp = @embedFile("program.zisp");
+const Program = try Compile(lisp);
 
 const TokenType = enum {
     Block,
-    Identifier
+    Identifier,
+    Number,
+    Operator,
 };
 
 const TokenData = union(TokenType) {
     Block: []const Token,
-    Identifier: []const u8
+    Identifier: []const u8,
+    Number: []const u8,
+    Operator: []const u8,
 };
 
 const Token = struct {
@@ -39,41 +43,6 @@ fn untilDelim(slice: []const u8, delims: []const u8) []const u8 {
     for (slice) |v, i| for (delims) |delim| if (delim == v) return slice[0..i];
     
     return slice;
-}
-
-fn validChar(comptime char: u8, comptime kind: enum { identifier, number, whitespace, string, delimiter } ) comptime bool {
-
-    return switch (kind) {
-        .identifier => switch (char) {
-            'a' ... 'z',
-            'A' ... 'Z',
-            '0' ... '9',
-            '-', '_', => true,
-            else => false
-        },
-        .number => switch (char) {
-            'e',
-            '0' ... '9',
-            '-', => true,
-            else => false
-        },
-        .whitespace => switch (char) {
-            ' ', '\n', '\r', '\t' => true,
-            else => false
-        },
-        .string => switch (char) {
-            'a' ... 'z',
-            'A' ... 'Z',
-            '0' ... '9',
-            '-', '_', ' ' => true,
-            else => false
-        },
-        .delimiter => switch (char) {
-            '(', ')'=> true,
-            else => false
-        }
-    };
-
 }
 
 
@@ -94,7 +63,7 @@ fn parseOperator(slice: []const u8) ?[]const u8 {
 
     if (i < slice.len) switch (slice[i]) {
         '(', ')', ' ', '\t', '\n' => {},
-        '\r' => @compileError("Carriage return is not part of the zig standard"),
+        '\r' => {},
         else => return null
     };
 
@@ -145,7 +114,7 @@ fn parseNumber(slice: []const u8) ?[]const u8 {
 
     if (i < slice.len) switch (slice[i]) {
         '(', ')', ' ', '\t', '\n' => {},
-        '\r' => @compileError("Carriage return is not part of the zig standard"),
+        '\r' => {},
         else => return null
     };
 
@@ -167,7 +136,7 @@ fn parseIdentifier(slice: []const u8) ?[]const u8 {
 
     if (i < slice.len) switch (slice[i]) {
         '(', ')', ' ', '\t', '\n' => {},
-        '\r' => @compileError("Carriage return is not part of the zig standard"),
+        '\r' => {},
         else => return null
     };
 
@@ -181,7 +150,6 @@ fn Compile(comptime code: []const u8) !type {
     var stack: []const *Token = &[0] *Token{};
     
     var totalTokens: usize = 0;
-    var stackLens: []const usize = &[0]usize{};
 
     var currentBlock: ?*Token = null;
 
@@ -227,58 +195,100 @@ fn Compile(comptime code: []const u8) !type {
         }
 
         if (code[i] == '(') {
+            var newToken = Token{ .line = 0, .number = i, .data = .{ .Block = &[0]Token{} }  };
+            if (currentBlock) |block| {
+                switch (block.data) {
+                    .Block => |*blockTokens| blockTokens.* = blockTokens.* ++ [1]Token{newToken},
+                    else => @compileError("currentBlock is not a block")
+                }
+                stack = stack ++ [1]*Token{block};
+            }
+            else topTokens = topTokens ++ [1]Token{newToken};
+
+            currentBlock = &newToken;
+            
             i += 1;
         }
         else if (code[i] == ')') {
+            
+            if (stack.len > 0) {
+                currentBlock = stack[stack.len - 1];
+                stack = stack[0..stack.len - 1];
+            }
+            else if(currentBlock) |_| currentBlock = null
+            else @compileError("Nothing to nest.");
+
             i += 1;
             continue :loop;
         }
         else if (parseOperator(code[i..])) |matched_operator| {
+            var newToken = Token{ .line = 0, .number = i, .data = .{ .Operator = matched_operator }  };
+            if (currentBlock) |block| {
+                switch (block.data) {
+                    .Block => |*blockTokens| blockTokens.* = blockTokens.* ++ [1]Token{newToken},
+                    else => @compileError("currentBlock is not a block")
+                }
+            }
+            
             i += matched_operator.len;
         }
         else if (parseNumber(code[i..])) |matched_number| {
+            var newToken = Token{ .line = 0, .number = i, .data = .{ .Number = matched_number } };
+            if (currentBlock) |block| {
+                switch (block.data) {
+                    .Block => |*blockTokens| blockTokens.* = blockTokens.* ++ [1]Token{newToken},
+                    else => @compileError("currentBlock is not a block")
+                }
+            }
             i += matched_number.len;
         }
         else if (parseIdentifier(code[i..])) |matched_identifier| {
+            var newToken = Token{ .line = 0, .number = i, .data = .{ .Identifier = matched_identifier } };
+            if (currentBlock) |block| {
+                switch (block.data) {
+                    .Block => |*blockTokens| blockTokens.* = blockTokens.* ++ [1]Token{newToken},
+                    else => @compileError("currentBlock is not a block")
+                }
+            }
             i += matched_identifier.len;
         }
         //TODO: A more explainatory error;
         else @compileError("Unexpected character or string: " ++ code[i]);
         totalTokens += 1;
     }
-    @compileLog(if (i == code.len) "Reached end of code" else "Did not reach end of code");
+    //@compileLog(if (i == code.len) "Reached end of code" else "Did not reach end of code");
     
-    return struct { const tokenCount = totalTokens; };
-//    return struct {
-//        const rawCodeString = code;
-//        const _arr = topTokens; 
-//        const total = totalTokens;
-//        const _arrStackLens = stackLens;
-//        const globals = .{};
-//        const procedures = .{};
-//
-//        fn printToken(outStream: var, token: Token) anyerror!void {
-//            switch(token.data) {
-//                .Identifier => |slice| try outStream.print("{}", .{slice}),
-//                .Block => |blockTokens| {
-//                    try outStream.print("(", .{});
-//                    for (blockTokens) |blockToken, i| {
-//                        try printToken(outStream, blockToken);
-//                        if (i < blockTokens.len - 1) try outStream.print(" ", .{});
-//                    }
-//                    try outStream.print(")", .{});
-//                },
-//            }
-//        }
-//
-//        pub fn printTo(outStream: var) anyerror!void {
-//            for (_arr) |token| {
-//                try printToken(outStream, token);
-//                try outStream.print("\n", .{});
-//            }
-//        }
-//    };
+    return struct {
+        const rawCodeString = code;
+        const outerTokens = topTokens; 
+        const tokenCount = totalTokens;
+        const globals = .{};
+        const procedures = .{};
+
+        fn printToken(outStream: var, token: Token) anyerror!void {
+            switch(token.data) {
+                .Identifier => |slice| try outStream.print("{}", .{slice}),
+                .Number => |slice| try outStream.print("{}", .{slice}),
+                .Operator => |slice| try outStream.print("{}", .{slice}),
+                .Block => |blockTokens| {
+                    try outStream.print("(", .{});
+                    for (blockTokens) |blockToken, index| {
+                        try printToken(outStream, blockToken);
+                        if (index < blockTokens.len - 1) try outStream.print(" ", .{});
+                    }
+                    try outStream.print(")", .{});
+                },
+            }
+        }
+
+        pub fn printTo(outStream: var) anyerror!void {
+            for (outerTokens) |token| {
+                try printToken(outStream, token);
+                try outStream.print("\n", .{});
+            }
+        }
+    };
 
 }
 
-const Program = Compile(lisp);
+
